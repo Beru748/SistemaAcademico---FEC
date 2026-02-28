a0f29fe68922270e7dbc8a6f93459b98f486e94e
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-02-27 23:17:50 -0500

    "feat: primer avance del menu asignatura "

--- SistemaAcademico/src/SistemaAcademico/Asignatura.java
+++ SistemaAcademico/src/SistemaAcademico/Asignatura.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package SistemaAcademico;
 
-/**
- *
- * @author COMPUMAX
- */
+
 public class Asignatura {
     private String Nombre;
     private String Codigo;
     private String Docente;
     private int Creditos;
 
-    public Asignatura(String Nombre, String Codigo, String Docente, int Creditos ){
-
-    }
 }
